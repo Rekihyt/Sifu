cef53eac62697f24af2c0163f103b20477160eff
Rekihyt <checkmateguy@gmail.com>
2023-05-25 18:48:57 -0700

    combine lexer and parser

--- src/ast.zig
+++ src/ast.zig
@@ -3,12 +3,22 @@ const Allocator = std.mem.Allocator;
 const ArrayList = std.ArrayList;
 const fsize = @import("util.zig").fsize;
 
-/// A single leaf node in Sifu's AST. It cannot contain other nodes. Keeps
-/// track of its position in the original source.
-pub const Term = struct {
+pub const Term = struct {};
+
+/// The Sifu AST primarily serves to abstract both infix operations and
+/// juxtaposition into `App`.
+pub const Ast = struct {
+    kind: Kind,
+    pos: usize,
+    len: usize,
+
     // May need to use `std.meta.fieldInfo(Term, .kind).field_type` if the
     // compiler complains about self-dependency
     pub const Kind = union(enum) {
+        /// The only node type that can contain other nodes. Because a term
+        /// cannot be contained by more than one app (besides being in a
+        /// nested app), they can be stored by value.
+        apps: []const Ast,
         sep: u8,
         val: []const u8,
         @"var": []const u8,
@@ -18,45 +28,4 @@ pub const Term = struct {
         double: f64,
         comment: []const u8,
     };
-    kind: Kind,
-    pos: usize,
-    len: usize,
-
-    pub fn create(allocator: Allocator, term: Term) !*Term {
-        const term_ptr = try allocator.create(Term);
-        const x: []const u8 = switch (term) {
-            .val, .@"var", .infix, .comment => |str| allocator.dupe(str),
-            .int, .double => |i| allocator.create(@TypeOf(i)),
-        };
-        _ = x;
-        term_ptr.* = term;
-        return term_ptr;
-    }
-};
-
-/// The Sifu AST primarily serves to abstract both infix operations and
-/// juxtaposition into `Apps`.
-pub const Ast = union(enum) {
-    term: *Term,
-    /// The only node type that can contain other nodes
-    apps: []const Ast,
-
-    pub fn create(allocator: Allocator, ast: Ast) !*Ast {
-        const ast_ptr = try allocator.create(Ast);
-        ast.* = ast;
-        return ast_ptr;
-    }
-
-    pub fn destroy(self: *Ast, allocator: Allocator) void {
-        switch (self.kind) {
-            .apps => |asts| for (asts) |ast| ast.destroy(allocator),
-            .val => {},
-            .@"var" => {},
-            .infix => {},
-            .int => {},
-            .double => {},
-            .comment => {},
-        }
-        allocator.destroy(self);
-    }
 };
--- src/parser.zig
+++ src/parser.zig
@@ -1,119 +1,388 @@
+/// The parser for Sifu tries to make as few decisions as possible. Mostly, it
+/// greedily lexes seperators like commas into their own Ast nodes, separates
+/// vars and vals based on the first character's case, and lexes numbers.
+/// There are no errors, any utf-8 text is parsable.
+///
+// The simple syntax of the language enables lexing and parsing at the same
+// time, so the parser lexes strings, ints, etc. into memory. Parsing always
+// begins with a new `App` node. Each token is
+// lexed, then a
+//
+const Parser = @This();
+
 const std = @import("std");
-const mem = std.mem;
-const ArrayList = std.ArrayList;
 const Allocator = std.mem.Allocator;
-const Arena = std.heap.ArenaAllocator;
-const Lexer = @import("lexer.zig");
-const Token = @import("token.zig");
+const ArenaAllocator = std.heap.ArenaAllocator;
+const panic = std.debug.panic;
+const Set = @import("util.zig").Set;
 const Ast = @import("ast.zig").Ast;
+const ArrayList = std.ArrayList;
+const fsize = @import("util.zig").fsize;
+const Error = Allocator.Error;
 
-/// Parse all tokens in the string `source`.
-pub fn parse(allocator: Allocator, source: []const u8) !*Ast {
-    var lexer = Lexer.init(source);
-    _ = lexer;
+/// Source code that is being tokenized
+source: []const u8,
+/// Current pos in the source
+pos: usize = 0,
+/// Current line in the source
+line: usize = 0,
+/// Current column in the source
+col: usize = 1,
+/// The allocator for each token, which will all be freed when the trie being
+/// lexed goes out of scope.
+arena: ArenaAllocator,
 
-    var arena = std.heap.ArenaAllocator.init(allocator);
-    errdefer arena.deinit();
-
-    const x = try Ast.create(allocator, Ast.Kind{ .val = "val1" });
-    errdefer x.destroy(allocator);
-    // var root = Ast.createApps(
-    //     allocator,
-    // );
-    // errdefer root.deinit();
-
-    // while (lexer.next()) |token| {
-    // try root.append(
-    //     switch (token.token_type) {
-    //         else => Ast{
-    //             .token = token,
-    //             .kind = Ast.Kind{
-    //                 .comment = try allocator.dupe(u8, token.str),
-    //             },
-    //         },
-    //     },
-    // );
-    // try Asts.append(parser.parseComment(next));
-    // }
-    return x;
-    // return root.toOwnedSlice();
+/// Creates a new parser using the given source code
+pub fn init(allocator: Allocator, source: []const u8) Parser {
+    return Parser{
+        .arena = ArenaAllocator.init(allocator),
+        .source = source,
+    };
 }
 
-/// Like the lexer, the parser tries to make as few decisions as possible.
-/// All tokens are accepted. Tokens like unclosed parenthesis are valid, as
-/// are operators missing any operands. These errors will be handled right
-/// after parsing, during the trie forming stage. The top-level construct is
-/// an a list of nodes separated by newlines, whose elements will be entries
-/// into a trie in the next phase.
-pub const Parser = struct {
-    /// Lexer that tokenized all tokens and is used to retrieve the next token
-    lexer: Lexer,
-};
+pub fn deinit(self: *Parser) void {
+    self.arena.deinit();
+}
+
+/// Caller owns returned memory.
+pub fn appList(self: *Parser) Error!?ArrayList(Ast) {
+    var apps_list = ArrayList(Ast).init(self.arena.allocator());
+
+    while (try self.nextToken()) |ast|
+        try apps_list.append(ast);
+
+    return apps_list;
+}
+
+/// Parses the source and returns the next sequence of terms forming an App,
+/// adding them to the arraylist. Allocates the
+fn nextToken(self: *Parser) Error!?Ast {
+    self.skipWhitespace();
+    const pos = self.pos;
+    const char = self.peek() orelse return null;
+
+    self.consume();
+    // If the type here is inferred, Zig may claim it depends on runtime val
+    const kind: Ast.Kind = switch (char) {
+        // Parse separators greedily. These are vals, but for efficiency stored as u8's.
+        '\n', ',', '.', ';', '(', ')', '{', '}', '[', ']', '"', '`' => .{ .sep = char },
+        '#' => .{ .comment = try self.comment(pos) },
+        '+', '-' => if (self.peek()) |next_char|
+            if (isDigit(next_char)) .{
+                .int = self.int(pos) catch unreachable,
+            } else .{ .infix = try self.infix(pos) }
+        else .{
+            // This block is here for readability, it could just be
+            // unified with the previous `else` block's call to `infix`
+            .infix = if (char == '+') "+" else "-",
+        },
+        else => if (isUpper(char) or char == '$') .{
+            .val = try self.val(pos),
+        } else if (isLower(char) or char == '_') .{
+            .@"var" = try self.@"var"(pos),
+        } else if (isDigit(char)) .{
+            .int = self.int(pos) catch panic(
+                \\Parser Error: Arbitrary width integers not supported yet:
+                \\ '{s}' at line {}, col {}"
+            , .{ self.source[pos..self.pos], self.line, self.col }),
+        } else if (isInfix(char)) .{
+            .infix = try self.infix(pos),
+        } else
+        // This is a debug error only, as we shouldn't encounter an error during lexing
+        panic(
+            "Parser Error: Unknown character '{c}' at line {}, col {}",
+            .{ char, self.line, self.col },
+        ),
+    };
+    return Ast{
+        .kind = kind,
+        .pos = pos,
+        .len = self.pos - pos,
+    };
+}
+
+/// Returns the next character but does not increase the Parser's position, or
+/// returns null if there are no more characters left to Astize.
+fn peek(self: Parser) ?u8 {
+    return if (self.pos < self.source.len)
+        self.source[self.pos]
+    else
+        null;
+}
+
+/// Advances one character, or panics (should only be called after `peek`)
+fn consume(self: *Parser) void {
+    if (self.peek()) |char| {
+        self.pos += 1;
+        switch (char) {
+            '\n' => {
+                self.col = 1;
+                self.line += 1;
+            },
+            else => self.col += 1,
+        }
+    } else @panic("Attempted to advance to next Ast but EOF reached");
+}
+
+/// Skips whitespace until a non-whitespace character is found. Not guaranteed
+/// to skip anything. Newlines are separators, and thus treated as Asts.
+fn skipWhitespace(self: *Parser) void {
+    while (self.peek()) |char| {
+        switch (char) {
+            ' ', '\t', '\r' => self.consume(),
+            else => break,
+        }
+    }
+}
+
+/// Reads the next characters as an val
+fn val(self: *Parser, pos: usize) Error![]const u8 {
+    while (self.peek()) |char|
+        if (isIdent(char))
+            self.consume()
+        else
+            break;
+
+    return try self.arena.allocator().dupe(u8, self.source[pos..self.pos]);
+}
+
+/// Reads the next characters as an var
+fn @"var"(self: *Parser, pos: usize) Error![]const u8 {
+    while (self.peek()) |char|
+        if (isIdent(char))
+            self.consume()
+        else
+            break;
+
+    return try self.arena.allocator().dupe(u8, self.source[pos..self.pos]);
+}
+
+/// Reads the next characters as an identifier
+fn infix(self: *Parser, pos: usize) Error![]const u8 {
+    while (self.peek()) |char|
+        if (isInfix(char))
+            self.consume()
+        else
+            break;
+
+    return try self.arena.allocator().dupe(u8, self.source[pos..self.pos]);
+}
+
+/// Reads the next characters as number
+fn int(self: *Parser, pos: usize) !usize {
+    while (self.peek()) |nextChar|
+        if (isDigit(nextChar))
+            self.consume()
+        else
+            break;
+
+    return if (std.fmt.parseUnsigned(usize, self.source[pos..self.pos], 10)) |i|
+        i
+    else |err| if (err == error.InvalidCharacter)
+        unreachable // we only consumed digits
+    else
+        err;
+}
+
+/// Reads the next characters as number
+fn float(self: *Parser, pos: usize) Error!fsize {
+    // A float is just and int with an optional period and int immediately
+    // after. This could still be implemented better though
+    _ = try self.int();
+    if (self.peek() == '.') {
+        self.consume();
+        _ = try self.int();
+    }
+    return try std.fmt.parseFloat(fsize, self.source[pos..self.pos], 10) catch
+        unreachable; // we only consumed digits, and maybe one decimal point
+}
+
+/// Reads a value wrappen in double-quotes from the current character
+fn string(self: *Parser, pos: usize) Ast {
+    while (self.peek()) |nextChar| {
+        self.consume(); // ignore the last double-quote
+        if (nextChar == '"')
+            break;
+    }
+
+    return try self.arena.allocator().dupe(u8, self.source[pos..self.pos]);
+}
+
+/// Reads until the end of the line or EOF
+fn comment(self: *Parser, pos: usize) Error![]const u8 {
+    while (self.peek()) |nextChar|
+        if (nextChar != '\n')
+            self.consume()
+        else
+            // Newlines that terminate comments are also tokens, so no
+            // `consume` here
+            break;
+
+    // `pos + 1` to ignore the '#'
+    return try self.arena.allocator().dupe(u8, self.source[pos + 1 .. self.pos]);
+}
+
+/// Returns true if the given character is a digit
+fn isDigit(char: u8) bool {
+    return switch (char) {
+        // Include underscores for spacing
+        '0'...'9', '_' => true,
+        else => false,
+    };
+}
+
+fn isUpper(char: u8) bool {
+    return switch (char) {
+        'A'...'Z' => true,
+        else => false,
+    };
+}
+
+fn isLower(char: u8) bool {
+    return switch (char) {
+        'a'...'z' => true,
+        else => false,
+    };
+}
+
+fn isIdent(char: u8) bool {
+    return switch (char) {
+        // zig fmt: off
+        ' ', '\n', '\t', '\r', ',', ';', ':', '.', '^', '*', '=', '<', '>',
+        '@', '$', '%', '&', '|', '/', '\\', '`', '[', ']', '(', ')', '{', '}',
+        '"',
+        => false,
+        // zig fmt: on
+        else => true,
+    };
+}
+
+fn isInfix(char: u8) bool {
+    return switch (char) {
+        // zig fmt: off
+        '.', ':', '-', '+', '=', '<', '>', '%', '^', '*', '&', '|', '/', '@',
+        => true,
+        // zig fmt: on
+        else => false,
+    };
+}
 
 const testing = std.testing;
-const expect = testing.expect;
-const expectEqual = testing.expectEqual;
-const expectEqualSlices = testing.expectEqualSlices;
 
-test "memory" {
-    const asts = try parse(testing.allocator, "Asd");
-    defer asts.destroy(testing.allocator);
+// TODO: add more tests after committing to using either spans or indices
+test "All Asts" {
+    const input =
+        \\Val1,5;
+        \\var1.
+        \\Infix -->
+        \\5 < 10.V
+        \\1 + 2.0
+        \\$strat
+        \\
+        \\10 == 10
+        \\10 != 9
+        \\"foo".len
+        \\[1, 2]
+        \\{"key":1}
+        \\// a comment
+        \\||
+        \\()
+    ;
+    const tests = &[_]Ast{
+        .{ .kind = .{ .val = "Val1" }, .pos = 0, .len = 4 },
+        .{ .kind = .{ .val = "," }, .pos = 4, .len = 1 },
+        .{ .kind = .{ .int = 5 }, .pos = 5, .len = 1 },
+        .{ .kind = .{ .val = ";" }, .pos = 6, .len = 1 },
+    };
+
+    var parser = Parser.init(testing.allocator, input);
+
+    for (tests) |unit| {
+        const next_term = (try parser.nextToken()).?;
+
+        switch (next_term.kind) {
+            .val, .@"var", .comment, .infix => |str| {
+                try testing.expectEqualStrings(unit.kind.val, str);
+            },
+            .int => |i| {
+                try testing.expectEqual(unit.kind.int, i);
+            },
+            else => {},
+        }
+        try testing.expectEqual(unit.pos, next_term.pos);
+        try testing.expectEqual(unit.len, next_term.len);
+        // TODO: uncomment when Zig 0.11
+        // try testing.expectEqualDeep(unit.kind, next_term.kind);
+    }
 }
 
-test "Parse val" {
-    const input = "FooBar";
+test "Vals" {
+    const val_strs = &[_][]const u8{
+        "A",
+        "Word-43",
+        "Word-asd-cxvlj_9182--+",
+        "Random123",
+        "Ssag-123+d",
+    };
 
-    const ast = try parse(testing.allocator, input);
-    errdefer testing.allocator.free(ast);
+    for (val_strs) |val_str| {
+        var parser = Parser.init(testing.allocator, val_str);
+        const next_term = (try parser.nextToken()).?;
+        // try std.io.getStdErr().writer().print("{}\n", .{next_term});
+        // try std.io.getStdErr().writer().print("{s}\n", .{val_str[next_term.val]});
+        try testing.expect(.val == next_term.kind); // Use == here to coerce union to enum
+        try testing.expectEqual(@as(?Ast, null), try parser.nextToken());
+    }
 
-    try expect(ast.len == 1);
-
-    try expect(ast[0].token.token_type == .val);
-    try expectEqualSlices(u8, ast[0].kind.val, input);
+    var parser = Parser.init(testing.allocator, "-Sd+ ++V"); // Should be -, Sd+, ++, V
+    try testing.expectEqualSlices(u8, "-", (try parser.nextToken()).?.kind.infix);
+    try testing.expectEqualSlices(u8, "Sd+", (try parser.nextToken()).?.kind.val);
+    try testing.expectEqualSlices(u8, "++", (try parser.nextToken()).?.kind.infix);
+    try testing.expectEqualSlices(u8, "V", (try parser.nextToken()).?.kind.val);
 }
 
-// test "Parse integer literal" {
-//     const input = "124";
-//     const tree = try parse(testing.allocator, input);
-//     defer tree.deinit();
+test "Vars" {
+    const varStrs = &[_][]const u8{
+        "a",
+        "word-43",
+        "word-asd-cxvlj_9182-",
+        "random123",
+        "_sd",
+    };
+    for (varStrs) |varStr| {
+        var parser = Parser.init(testing.allocator, varStr);
+        defer parser.deinit();
+        try testing.expect(.@"var" == (try parser.nextToken()).?.kind);
+        try testing.expectEqual(@as(?Ast, null), try parser.nextToken());
+    }
 
-//     try expect(tree.Asts.len == 1);
-//     const literal = tree.Asts[0].expression.value.int_lit;
-//     try expect(literal.token.token_type == .integer);
-//     try expect(literal.value == 124);
-// }
+    const notVarStrs = &[_][]const u8{
+        "\n\t\r Asdf,",
+        "-Word-43-",
+        "Word-asd-cxvlj_9182-",
+        "Random_123_",
+    };
+    for (notVarStrs) |notVarStr| {
+        var parser = Parser.init(testing.allocator, notVarStr);
+        while (try parser.nextToken()) |term| {
+            _ = term;
+            // try testing.expect(.@"var" != term.kind);
+        }
+    }
+}
 
-// test "Parse infix expressions - identifier" {
-//     const input = "foobar + foobarz";
-//     const tree = try parse(testing.allocator, input);
-//     defer tree.deinit();
-
-//     try expect(tree.Asts.len == 1);
-//     try expectEqualSlices(u8, "foobar", infix.left.identifier.value);
-//     try expectEqualSlices(u8, "foobarz", infix.right.identifier.value);
-//     try expect(infix.operator == .add);
-// }
-
-// test "String expression" {
-//     const input = "\"Hello, world\"";
-
-//     const tree = try parse(testing.allocator, input);
-//     defer tree.deinit();
-
-//     try expect(tree.Asts.len == 1);
-
-//     const string = tree.Asts[0].expression.value.string_lit;
-//     try expectEqualSlices(u8, "Hello, world", string.value);
-// }
-
-// test "Comment expression" {
-//     const input = "//This is a comment";
-
-//     const tree = try parse(testing.allocator, input);
-//     defer tree.deinit();
-
-//     try expect(tree.Asts.len == 1);
-
-//     const comment = tree.Asts[0].comment;
-//     try testing.expectEqualStrings("This is a comment", comment.value);
-// }
+test "simple App of vals" {
+    var parser = Parser.init(testing.allocator, "Aa Bb Cc");
+    const asts = &[_]Ast{
+        Ast{ .kind = .{ .val = "Aa" }, .pos = 0, .len = 2 },
+        Ast{ .kind = .{ .val = "Bb" }, .pos = 3, .len = 2 },
+        Ast{ .kind = .{ .val = "Cc" }, .pos = 6, .len = 2 },
+    };
+    const app_list = (try parser.appList()).?;
+    for (asts) |ast, i| {
+        try testing.expectEqualStrings(ast.kind.val, app_list.items[i].kind.val);
+        try testing.expect(.val == app_list.items[i].kind);
+        try testing.expectEqual(ast.pos, app_list.items[i].pos);
+        try testing.expectEqual(ast.len, app_list.items[i].len);
+    }
+}
